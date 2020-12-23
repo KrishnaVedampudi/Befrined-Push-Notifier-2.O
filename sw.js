bcaee7974a41fbeeb7f7365bc8367e3b940f81f3
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-23 17:02:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -107,5 +107,31 @@ OneSignal.sendSelfNotification(
     url: 'https://example.com/?_osp=do_not_open'
   }]
 );
+function isPushNotificationsEnabledVerbose() {
+    console.log('isPushNotificationsEnabledVerbose()');
+    Promise.all([
+            OneSignal.isPushNotificationsEnabled(),
+            OneSignal.getUserId(),
+            OneSignal.getRegistrationId(),
+            OneSignal.getNotificationPermission(),
+            OneSignal.isOptedOut(),
+            OneSignal.context.serviceWorkerManager.getActiveState()
+        ])
+        .then(([isSubscribed, userId, registrationId, notificationPermission, optedOut, serviceWorkerActive]) => {
+            console.log('Is Completely Subscribed:', isSubscribed);
+            console.log('');
+            console.log('What is our OneSignal user ID?', userId);
+            console.log('What is our push subscription token?', registrationId);
+            console.log('What is the notification permission status?', notificationPermission);
+            console.log('Are you manually opted out?', optedOut);
+            console.log("Is a service worker registered and active? (should be false on Safari, otherwise should be 'Worker A (Main)')?", serviceWorkerActive);
+            console.log('What is the current URL of this page?', location.href);
+            console.log("What environment does OneSignal think it's in?", OneSignal.sdkEnvironment.getWindowEnv());
+        })
+        .catch(e => {
+            console.error("Issue determining whether push is enabled:", e);
+        });
+}
+isPushNotificationsEnabledVerbose();
  
               
