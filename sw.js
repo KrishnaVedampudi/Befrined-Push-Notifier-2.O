d84835f450250238e1173d87226239510f47ee2b
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-25 10:45:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,17 +22,15 @@ function isPushNotificationsEnabledVerbose() {
             OneSignal.getUserId(),
             OneSignal.getRegistrationId(),
             OneSignal.getNotificationPermission(),
-            OneSignal.isOptedOut(),
-            OneSignal.context.serviceWorkerManager.getActiveState()
+            OneSignal.isOptedOut()          
         ])
-        .then(([isSubscribed, userId, registrationId, notificationPermission, optedOut, serviceWorkerActive]) => {
+        .then(([isSubscribed, userId, registrationId, notificationPermission, optedOut]) => {
             console.log('Is Completely Subscribed:', isSubscribed);
             console.log('');
             console.log('What is our OneSignal user ID?', userId);
             console.log('What is our push subscription token?', registrationId);
             console.log('What is the notification permission status?', notificationPermission);
-            console.log('Are you manually opted out?', optedOut);
-            console.log("Is a service worker registered and active? (should be false on Safari, otherwise should be 'Worker A (Main)')?", serviceWorkerActive);
+            console.log('Are you manually opted out?', optedOut);            
             console.log('What is the current URL of this page?', location.href);
             console.log("What environment does OneSignal think it's in?", OneSignal.sdkEnvironment.getWindowEnv());
         })
