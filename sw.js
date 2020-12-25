706e78e7ae493047c24b93c9e98162e68b9193b7
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-25 11:26:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,35 +13,7 @@ window.OneSignal = window.OneSignal || [];
      "url": "https://www.google.com" 
        }
     });
-  });  
-
-function isPushNotificationsEnabledVerbose() {
-    console.log('isPushNotificationsEnabledVerbose()');
-    Promise.all([
-            OneSignal.isPushNotificationsEnabled(),
-            OneSignal.getUserId(),
-            OneSignal.getRegistrationId(),
-            OneSignal.getNotificationPermission(),
-            OneSignal.isOptedOut()          
-        ])
-        .then(([isSubscribed, userId, registrationId, notificationPermission, optedOut]) => {
-            console.log('Is Completely Subscribed:', isSubscribed);
-            console.log('');
-            console.log('What is our OneSignal user ID?', userId);
-            console.log('What is our push subscription token?', registrationId);
-            console.log('What is the notification permission status?', notificationPermission);
-            console.log('Are you manually opted out?', optedOut);            
-            console.log('What is the current URL of this page?', location.href);
-            console.log("What environment does OneSignal think it's in?", OneSignal.sdkEnvironment.getWindowEnv());
-        })
-        .catch(e => {
-            console.error("Issue determining whether push is enabled:", e);
-        });
-}
- 
-
-isPushNotificationsEnabledVerbose();
-OneSignal.log.setLevel('warn');
+  });
 function prompt()
 {
   OneSignal.push(function() {
@@ -75,13 +47,14 @@ function prompt()
                }]                 
               const body = {    
                "app_id": "2e7a2add-017c-4365-8d3c-9ffe6b62582f",
-               "include_player_ids":[noti],
-               "headings" : {"en": "A job request!"},                    
-               "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},
+               "channel_for_external_user_ids": "push",
+               "contents": {"en": "Hello, someone is waiting for you!!!! If you want to aceept the job request, click on I am willing"},                
+               "headings" : {"en": "A job request!"},               
+               "include_external_user_ids":[noti],             
                "web_buttons": web_buttons
               }           
               fetch('https://onesignal.com/api/v1/notifications', {
-                      method: 'POST',
+                     method: 'POST',
                      body: JSON.stringify(body),
                       headers: {               
                         "Content-type": "application/json; charset=utf-8",
