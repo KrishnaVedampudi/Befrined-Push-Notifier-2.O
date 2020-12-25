bc85101ab1a8107f7beccfe962c70727fb96cb35
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-25 10:43:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,6 +14,33 @@ window.OneSignal = window.OneSignal || [];
        }
     });
   });  
+
+function isPushNotificationsEnabledVerbose() {
+    console.log('isPushNotificationsEnabledVerbose()');
+    Promise.all([
+            OneSignal.isPushNotificationsEnabled(),
+            OneSignal.getUserId(),
+            OneSignal.getRegistrationId(),
+            OneSignal.getNotificationPermission(),
+            OneSignal.isOptedOut(),
+            OneSignal.context.serviceWorkerManager.getActiveState()
+        ])
+        .then(([isSubscribed, userId, registrationId, notificationPermission, optedOut, serviceWorkerActive]) => {
+            console.log('Is Completely Subscribed:', isSubscribed);
+            console.log('');
+            console.log('What is our OneSignal user ID?', userId);
+            console.log('What is our push subscription token?', registrationId);
+            console.log('What is the notification permission status?', notificationPermission);
+            console.log('Are you manually opted out?', optedOut);
+            console.log("Is a service worker registered and active? (should be false on Safari, otherwise should be 'Worker A (Main)')?", serviceWorkerActive);
+            console.log('What is the current URL of this page?', location.href);
+            console.log("What environment does OneSignal think it's in?", OneSignal.sdkEnvironment.getWindowEnv());
+        })
+        .catch(e => {
+            console.error("Issue determining whether push is enabled:", e);
+        });
+}
+isPushNotificationsEnabledVerbose();
 OneSignal.log.setLevel('warn');
 function prompt()
 {
